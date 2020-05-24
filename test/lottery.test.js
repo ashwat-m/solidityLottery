2db50f9384b354fad1c09e7f4ca12b0b218cbe82
Ashwat Mahendran <am123@snu.edu.in>
2020-05-24 13:48:22 +0530

    added the players array reset test case

--- test/lottery.test.js
+++ test/lottery.test.js
@@ -89,13 +89,14 @@ describe('Lottery Contract', () => {
     });
 
     const initialBalance = await web3.eth.getBalance(accounts[0]);
-
     await lottery.methods.pickWinner().send({from: accounts[0]});
-
     const finalBalance = await web3.eth.getBalance(accounts[0]);
+    const difference = finalBalance - initialBalance;    
+    assert(difference > web3.utils.toWei('1.8', 'ether')); 
 
-    const difference = finalBalance - initialBalance;
-    
-    assert(difference > web3.utils.toWei('1.8', 'ether'));
+    players = await lottery.methods.getPlayers().call({
+      from: accounts[0]
+    });
+    assert.equal(0, players.length);
   });
 });
